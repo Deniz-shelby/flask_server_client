ed8831591aa3b5ae1deb6dc2f5749f0936959deb
Deniz-shelby <d.elci@hotmail.de>
2021-07-15 17:56:15 +0200

    Update server.py

--- client_server_working/server.py
+++ client_server_working/server.py
@@ -7,40 +7,44 @@ import torch.nn.functional as F
 model = torch.load("Best_model2.pth")
 
 HEADERSIZE = 10
+PORT = 5551
+#IP = '127.0.0.2'
 
 s = socket(AF_INET, SOCK_STREAM)
-s.bind(('', 5551))
-s.listen(5)
-
-# while True:
-full_msg = b''
-new_msg = True
-connection, address = s.accept()
-print("Connected from", address)
-
+s.bind(('', PORT))
 while True:
-    msg = connection.recv(16)
-    if new_msg:
-        # print(f'new message legnth:{msg[:HEADERSIZE]}')
-        msg_len = int(msg[:HEADERSIZE])
-        new_msg = False
-    full_msg += msg
+    s.listen(5)
+    full_msg = b''
+    new_msg = True
+    connection, address = s.accept()
+    print("Connected from", address)
+    try:
 
-    if len(full_msg) - HEADERSIZE == msg_len:
-        # print('Full msg recvd')
+        while True:
+            msg = connection.recv(16)
+            if new_msg:
+                # print(f'new message legnth:{msg[:HEADERSIZE]}')
+                msg_len = int(msg[:HEADERSIZE])
+                new_msg = False
+            full_msg += msg
 
-        img = pickle.loads(full_msg[HEADERSIZE:])
+            if len(full_msg) - HEADERSIZE == msg_len:
+                # print('Full msg recvd')
 
-        new_msg = True
-        full_msg = b''
-        if new_msg:
-            print("yes")
-            print(type(img))
-            img = np.array(img, dtype="float32")
-            tensor_img = torch.from_numpy(img).reshape(784, 1)
-            tensor_img = F.normalize(tensor_img)
-            tensor_img = tensor_img.view((-1, 784))
-            print(torch.argmax(model(tensor_img)))
-            pred = str(torch.argmax(model(tensor_img)).item()).encode()
-            connection.send(pred)
+                img = pickle.loads(full_msg[HEADERSIZE:])
+
+                new_msg = True
+                full_msg = b''
+                if new_msg:
+                    print("yes")
+                    print(type(img))
+                    img = np.array(img, dtype="float32")
+                    tensor_img = torch.from_numpy(img).reshape(784, 1)
+                    tensor_img = F.normalize(tensor_img)
+                    tensor_img = tensor_img.view((-1, 784))
+                    print(torch.argmax(model(tensor_img)))
+                    pred = str(torch.argmax(model(tensor_img)).item()).encode()
+                    connection.send(pred)
+    except:
+        continue
 
