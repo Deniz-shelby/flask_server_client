e74beb4715b8d9faa6760f26abcf5c15f16d59e3
Deniz-shelby <d.elci@hotmail.de>
2021-07-15 14:42:20 +0200

    Update client.py

--- jons_client_server/client.py
+++ jons_client_server/client.py
@@ -2,12 +2,12 @@ import socket
 
 HOST = '127.0.0.1'    
 PORT = 5005
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-print('Client connected.')
-while True:
-    user_input = str(input())  
-    if user_input == "exit":  
-        break
-    mess = user_input.encode()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP
+s.connect((HOST, PORT)) #connect to server
+print('Client connected.') #print message to console
+while True: #loop forever
+    user_input = str(input()) #get user input  
+    if user_input == "exit":  #if user input is exit, close connection and exit loop
+        break #exit loop
+    mess = user_input.encode() #encode user input as bytes
     s.sendall(mess)
