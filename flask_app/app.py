e9e00001ae94e82da84c9c7312d2a3c828b2d540
Deniz-shelby <d.elci@hotmail.de>
2021-07-15 17:56:19 +0200

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,22 +1,44 @@
-from flask import Flask, render_template, request 
-from werkzeug import secure_filename
+from flask import Flask, render_template, Response
+import socket
+import cv2 as cv
+import pickle
 
+#import client
 
 app = Flask(__name__)
 
 @app.route('/')
-def upload_file():
+def index():
    return render_template('index.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-		
-if __name__ == '__main__':
-   app.run(debug = True)
+
+def client():
+   print('fuckoff')
+   HEADERSIZE = 10
+   PORT = 5551
+   #Ip ='127.0.0.2'
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object
+   s.connect((socket.gethostname(), PORT)) # Connect to the server
+
+   while True:
+      # user_input = str(input()) # Receive the data from the server
+      # if user_input == "exit":    # Check if the user wants to exit
+      #    break
+      # elif user_input == "send": # Check if the user wants to send a file
+      img = cv.imread("no.jpg", 0)
+
+      msg = pickle.dumps(img)
+      msg = bytes(f'{len(msg):<{HEADERSIZE}}', 'utf-8') + msg # Create a header
+      s.send(msg)
+      msg = s.recv(1024)
+      print(msg.decode())
+      prediction = msg.decode()
+      break
+   s.close()
+   
+
+# @app.route('/send_client')
+def send_client():
+   return Response(client())
 
 if __name__ == "__main__":
     app.run(debug=True)
