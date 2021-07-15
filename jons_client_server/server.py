cfc0bc9f5b1f7c1e8e30127dae774fbacbddb3ae
Deniz-shelby <d.elci@hotmail.de>
2021-07-15 13:04:02 +0200

    Create server.py

---
+++ jons_client_server/server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = '127.0.0.1'    
+PORT = 5005
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP SOCKET
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()  
+print('Client connected: ', addr)
+
+
+while True:
+    data = conn.recv(1024)
+    if not data or data == "":
+        break
+    decoded_data = data.decode()
+    print(decoded_data)
